c389b3e00a6d0bbace984c5c17c4c18c5d253ed1
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2021-04-17 21:41:19 -0300

    black para codigo y previene node.body no lista

--- auxnb.py
+++ auxnb.py
@@ -13,6 +13,7 @@ from IPython.display import display, Markdown
 from ast import Expr, Str, walk, parse
 from astunparse import unparse
 from inspect import getsource
+from black import format_str, FileMode
 
 
 def code(objeto):
@@ -20,20 +21,30 @@ def code(objeto):
     Genera un _code fencing_ en Markdown y ordena su visualización
 
     Se espera que el *objeto* tenga asociado un archivo con su
-    código fuente. El resultado puede diferir del código literal ya 
-    que se analizará el árbol sintáctico abstracto, se eliminarán 
-    los _docstring_ y se reconstruirá el código. Se creará un salida 
+    código fuente. El resultado puede diferir del código literal ya
+    que se analizará el árbol sintáctico abstracto, se eliminarán
+    los _docstring_ y se reconstruirá el código. Se creará un salida
     del tipo :class:`IPython.core.display.Markdown`
 
     :param objeto: entidad de la que se desea el código fuente
     :type objeto: object
     """
     tree = parse(getsource(objeto))
-    for node in filter(lambda x: hasattr(x, "body"), walk(tree)):
-        node.body = list(
-            filter(
-                lambda x: not isinstance(x, Expr) or not isinstance(x.value, Str),
-                node.body,
+    for node in walk(tree):
+        try:
+            node.body = list(
+                filter(
+                    lambda x: not isinstance(x, Expr)
+                    or not isinstance(x.value, Str),
+                    node.body,
+                )
             )
+        except BaseException:
+            pass
+    display(
+        Markdown(
+            "```python\n"
+            + format_str(unparse(tree).strip(), mode=FileMode())
+            + "\n```"
         )
-    display(Markdown("```python\n" + unparse(tree).strip() + "\n```"))
+    )
