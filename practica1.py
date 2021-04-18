3ac074712ef114120a9188c3a5c24d0a4cf68a4d
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2021-04-17 21:41:58 -0300

    Función escalón y generador de muestras

--- practica1.py
+++ practica1.py
@@ -11,6 +11,7 @@
     en el ciclo lectivo de 2021.
 """
 
+
 def duration(signal):
     """
     Calcula la duración de una señal a partir de sus muestras y frecuencia
@@ -22,6 +23,7 @@ def duration(signal):
     """
     return (len(signal[0]) - 1) / signal[1]
 
+
 def med(signal):
     """
     Calcula el valor medio de una señal discretizada
@@ -32,3 +34,50 @@ def med(signal):
     :rtype: float
     """
     return sum(signal[0]) / len(signal[0])
+
+
+def heavi(t):
+    """
+    Función escalón de Heaviside
+
+    Implementa la función escalón de Heaviside con
+    semicontinuidad superior (:func:`heavi`(0) = 1)
+
+    :param t: parámetro de la función
+    :type t: Union[float, int, complex]
+    :return: devuelve 1 ó 0
+    :rtype: int
+    """
+    return 1 if t >= 0 else 0
+
+
+def sample(func, frec, init, dur):
+    """
+    Generación de una señal a partir de una función
+
+    Genera una terna que representa a una señal muestreada
+    apartir de una función. La función *func* de estar definida en
+    el mismo marco temporal que el resto de las funciones y
+    debe aceptar un flotante como argumento. La frecuencia
+    *frec* debe estar expresada de hercios, el tiempo de inicio
+    *init* de la señal muestreada debe ser expresado en segundos
+    con respecto al marco temporal compartido por todas las
+    funciones. La duración *dur* debe expresarse en segundos.
+    Una duración de cero segundo tomará una muestra puntual.
+
+    :param func: función de la que se desea tomar una muestra
+    :type func: function
+    :param frec: frecuencia de muestreo
+    :type frec: float
+    :param init: tiempo de inicio del muestreo
+    :type init: float
+    :param dur: duración del muestreo
+    :type dur: float
+    :return: una terna que representa una señal muestreada
+    :rtype: tuple(list(), float, float)
+    """
+    return (
+        [func(init + t * frec) for t in range(int(frec * dur) + 1)],
+        frec,
+        init,
+    )
